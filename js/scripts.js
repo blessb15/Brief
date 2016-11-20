991b7377178c5bf93881f6a1dae7a261cbf2e7f0
blessb15 <blakebless7@gmail.com>
2016-11-19 19:49:07 -0800

    fix car changer bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 $(Document).ready(function(){
 
-  console.log("hey");
-
-  var pages = ['.page-1', '.page-2', 'page-3'];
+  var pages = ['.page-1', '.page-2', '.page-3'];
 
   $('.footer-nav-down').click(function(){
     for(var i = 0; i < pages.length; i++){
+      console.log(pages.length);
+      console.log(i);
       if ($(pages[i]).hasClass('active')){
         $(pages[i]).removeClass('active');
-        if(!(pages[i+1] >= pages.length)){
-          $(pages[i+1]).addClass('active');
+        if(i + 1 < pages.length){
+          $(pages[i + 1]).addClass('active');
           break;
-        } else if (pages[i+1] > pages.length) {
+        } else if (i + 1 >= pages.length) {
           $(pages[0]).addClass('active');
           break;
         }
