518cc61e6fa805739b4a33d9eed1c133bb986dca
Blake Bless <blakebless7@gmail.com>
2016-12-05 18:46:06 -0800

    fix issue with car cicle

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,18 @@ $(Document).ready(function(){
 
   console.log("hey");
 
-  var pages = ['.page-1', '.page-2', 'page-3'];
+  var pages = ['.page-1', '.page-2', '.page-3'];
 
   $('.footer-nav-down').click(function(){
     for(var i = 0; i < pages.length; i++){
       if ($(pages[i]).hasClass('active')){
-        $(pages[i]).removeClass('active');
-        if(!(pages[i+1] >= pages.length)){
+        if(!(i+1 >= pages.length)){
           $(pages[i+1]).addClass('active');
+          $(pages[i]).removeClass('active');
           break;
-        } else if (pages[i+1] > pages.length) {
+        } else if (i+1 >= pages.length) {
           $(pages[0]).addClass('active');
+          $(pages[i]).removeClass('active');
           break;
         }
       }
